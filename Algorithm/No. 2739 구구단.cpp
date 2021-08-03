9e9729e366b3aa3c35dbcfc0f94d69fe16a6054f
Serendipity-Song <thdalwh3867@gmail.com>
2021-08-03 13:33:33 +0900

    for 문 풀기-초기값, 조건값, 더하기값

---
+++ Algorithm/No. 2739 구구단.cpp
@@ -0,0 +1,17 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int N;
+	cin >> N;
+
+	int i;
+
+	for (i = 1; i < 10; i++) {
+		cout << N << " * "  << i << " = " << N * i << endl;
+	}
+
+	return 0;
+}
