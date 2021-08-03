4c211af1549804899cefa24a1e7df0b576ba0428
Serendipity-Song <thdalwh3867@gmail.com>
2021-08-03 14:29:19 +0900

    1부터 n까지 더하기 (for 이용)

---
+++ Algorithm/No. 8393 N까지의 합.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+        
+        int i;
+        int x = 0;
+
+        for (i=0; n>=i; i++) {
+            
+            
+            x = x + i;
+            
+            
+        }
+        cout << x << endl;
+
+       
+}
