2eb409101ff7441090157840f36410377c6d8a8f
Serendipity-Song <thdalwh3867@gmail.com>
2021-08-06 12:57:04 +0900

    X보다 작은 수

--- Algorithm/No. 10871 X보다 작은 수.cpp
+++ Algorithm/No. 10871 X보다 작은 수.cpp
@@ -3,22 +3,16 @@
 using namespace std;
 
 
+int a = 0, b = 0, c = 0;
+
+
 int main() {
+    cin >> a >> b;
+    for (int i = 1; i <= a; i++) {
+        cin >> c;
+        if (c < b) {
+            cout << c << " ";
+        }
 
-	int N;
-	int X;
-	cin >> N >> X;	// N과 X 입력
-
-	int arr[N];	// N칸의 배열 생성
-
-	for (int i = 0; i < N; i++) {
-		cin >> arr[i];		// 배열에 N개의 수를 저장
-	}
-
-	for (int i = 0; i < N; i++) {
-		// i번째 원소가 X보다 작다면 출력
-		if (arr[i] < X) {
-			cout << arr[i] << " ";
-		}
-	}
+    }
 }
