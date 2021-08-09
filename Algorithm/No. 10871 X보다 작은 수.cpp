a822a0f4f1ca45f13785b48e17a1958f9b5a3c21
Serendipity-Song <thdalwh3867@gmail.com>
2021-08-09 22:16:53 +0900

    0809

--- Algorithm/No. 10871 X보다 작은 수.cpp
+++ Algorithm/No. 10871 X보다 작은 수.cpp
@@ -7,12 +7,17 @@ int a = 0, b = 0, c = 0;
 
 
 int main() {
-    cin >> a >> b;
-    for (int i = 1; i <= a; i++) {
-        cin >> c;
-        if (c < b) {
-            cout << c << "  ";
-        }
+	cin >> a >> b;
+	// i 는 순서다. 1부터 a까지 증가하는!
 
-    }
+	for (int i = 1; i <= a; i++) {
+		cin >> c;
+		if (c<b) {
+			cout << c<< " ";
+		}
+		
+
+	}
+
 }
+
