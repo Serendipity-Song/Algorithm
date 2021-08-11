22c7730807521595b6c73abef508f1e3464ac511
Serendipity-Song <thdalwh3867@gmail.com>
2021-08-11 16:16:48 +0900

    틀 잡기

---
+++ Algorithm/No. 1110 앞뒤숫자 더해서 원상태로 만들기.cpp
@@ -0,0 +1,28 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	while (true) {
+
+		int x;
+		int y;
+		cin >> x>>y;
+
+		if (x < 10) {
+			x = 0 << x;
+		}
+
+		for (int i = 0;;i++) {
+			int a,b;
+
+			a = x % 10;
+			b = x / 10;
+
+
+		}
+
+
+	}
+}
