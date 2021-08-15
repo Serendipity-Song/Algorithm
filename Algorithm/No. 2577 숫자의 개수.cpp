5abdfca5cd6a3611009573892e6a44044accfaf4
Serendipity-Song <thdalwh3867@gmail.com>
2021-08-15 17:08:18 +0900

    숫자의 개수 1. 곱해서 출력하기

---
+++ Algorithm/No. 2577 숫자의 개수.cpp
@@ -0,0 +1,22 @@
+
+#include <iostream>
+using namespace std;
+
+
+int A;
+int B;
+int C;
+
+int N[];
+int Result;
+
+int main() {
+
+	cin >> A;
+	cin >> B;
+	cin >> C;
+
+	Result = A * B * C;
+	cout << Result;
+
+}
