b9c7ceccdbc7d9dd0eb40204a0b8dc3aeb373ed5
Serendipity-Song <thdalwh3867@gmail.com>
2021-08-20 12:22:24 +0900

    한수 함수 완료!

--- Algorithm/No. 1065 한수.cpp
+++ Algorithm/No. 1065 한수.cpp
@@ -2,17 +2,39 @@
 #include <iostream>
 using namespace std;
 
-int Han(int n);
+//함수를 실행하자
 
-void main() {
+int Han(int n) {
+	int i, hundred, ten, one;
+	int cnt = 0;
+	if (n < 100)
+		return n;
 
+	else {
+		for (i = 100; i <= n; i++) {
+			hundred = i / 100;
+			ten = i % 100 / 10;
+			one = i % 10;
+
+			if ((hundred - ten) == (ten - one))
+				cnt++;
+		}
+
+		return (99 + cnt);
+	}
 
 }
 
+//프로그램을 돌리자
+int main() {
+
+	int n;
+	cin >> n;
+	Han(n);
+	cout << Han(n) << endl;
+	return 0;
+
+}
 
 
 //함수를 정의하자
-int Han(int n) {
-
-	
-}
