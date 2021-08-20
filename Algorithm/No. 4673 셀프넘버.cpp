be927885ce20d6fa87d95072539a583d195dd3b9
Serendipity-Song <thdalwh3867@gmail.com>
2021-08-20 11:27:19 +0900

    함수-한수 시작

--- Algorithm/No. 4673 셀프넘버.cpp
+++ Algorithm/No. 4673 셀프넘버.cpp
@@ -35,44 +35,87 @@
 
 
 
+//
+//#include <iostream>
+//using namespace std;
+//
+//
+//
+//int main(void) {
+//    int arr[10000] = { 1, };
+//    int temp;
+//    for (int i = 1; i < 10000; i++) {
+//        if (i < 10) {
+//            arr[i + i] = 1;
+//            continue;
+//        }
+//
+//
+//        else if (i < 100) {
+//            arr[i + i / 10 + i % 10] = 1;
+//            continue;
+//        }
+//
+//        else if (i < 1000) {
+//            arr[i + i / 100 + i % 100 / 10 + i % 10] = 1;
+//            continue;
+//        }
+//
+//
+//        else if (i < 10000) {
+//            temp = i + i / 1000 + i % 1000 / 100 + i % 100 / 10 + i % 10;
+//            if (temp < 10000) {
+//                arr[temp] = 1;
+//                continue;
+//            }
+//        }
+//    }
+//
+//
+//    for (int i = 1; i < 10000; i++)
+//        if (!arr[i])
+//            cout << i << "\n";
+//}
+
+
 
 #include <iostream>
+
 using namespace std;
 
+int d(int n) {
+    int sum = n;
+
+    while (n != 0) {
+        sum += n % 10;
+        n = n / 10;
+    }
+
+    return sum;
+}
 
 
-int main(void) {
-    int arr[10000] = { 1, };
-    int temp;
-    for (int i = 1; i < 10000; i++) {
-        if (i < 10) {
-            arr[i + i] = 1;
-            continue;
-        }
+int main() {
 
+    bool arr[10001];
+    int tmp;
 
-        else if (i < 100) {
-            arr[i + i / 10 + i % 10] = 1;
-            continue;
-        }
+    for (int i = 1; i <= 10000; i++) {
+        arr[i] = true;
+    }
 
-        else if (i < 1000) {
-            arr[i + i / 100 + i % 100 / 10 + i % 10] = 1;
-            continue;
-        }
-
-
-        else if (i < 10000) {
-            temp = i + i / 1000 + i % 1000 / 100 + i % 100 / 10 + i % 10;
-            if (temp < 10000) {
-                arr[temp] = 1;
-                continue;
-            }
+    for (int i = 1; i <= 10000; i++) {
+        tmp = d(i);
+        if (tmp <= 10000) {
+            arr[tmp] = false;
         }
     }
 
-
-    for (int i = 1; i < 10000; i++)
-        if (!arr[i])
+    for (int i = 1; i <= 10000; i++) {
+        if (arr[i]) {
             cout << i << "\n";
+        }
+    }
+
+    return 0;
 }
