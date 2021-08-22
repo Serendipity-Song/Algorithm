5c585d18791849e72ee8719809715da9cef538d0
Serendipity-Song <thdalwh3867@gmail.com>
2021-08-22 10:12:38 +0900

    X보다 작은 수

--- Algorithm/소스.cpp
+++ Algorithm/소스.cpp
@@ -2,15 +2,22 @@
 #include <iostream>
 using namespace std;
 
+
 int a = 0, b = 0, c = 0;
+
 
 int main() {
 	cin >> a >> b;
+	// i 는 순서다. 1부터 a까지 증가하는!
 
 	for (int i = 1; i <= a; i++) {
 		cin >> c;
 		if (c < b) {
 			cout << c << " ";
 		}
+
+
 	}
+
 }
+
