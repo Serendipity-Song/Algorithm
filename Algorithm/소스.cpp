8bca8a78723954efa1d77e2d353dbf92fc22125a
Serendipity-Song <thdalwh3867@gmail.com>
2021-08-22 10:00:26 +0900

    c++ 시간 줄이기- cin.tie와 syncstdio

--- Algorithm/소스.cpp
+++ Algorithm/소스.cpp
@@ -1,16 +1,21 @@
-
-#include<iostream>
+//c++에서 시간 줄이기
+#include <iostream>
 using namespace std;
 
+
 int main() {
+
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
 
 	int T;
 	cin >> T;
 
-	for (;0<T;T--) {
-		int a, b;
-		cin >> a >> b;
-		cout << a + b << endl;
+		for (; T > 0; T--){
+	
+			int a,b;
+			cin >> a >> b;
 
-	}
+			cout << a + b << "\n";
+		}
 }
