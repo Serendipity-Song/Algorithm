ca2bfa4f38c0892683d5441f575e03bd74951afb
Serendipity-Song <thdalwh3867@gmail.com>
2021-08-22 10:16:56 +0900

    윤년 계산하기

--- Algorithm/소스.cpp
+++ Algorithm/소스.cpp
@@ -1,23 +1,18 @@
 
+
 #include <iostream>
 using namespace std;
 
 
-int a = 0, b = 0, c = 0;
-
-
 int main() {
-	cin >> a >> b;
-	// i 는 순서다. 1부터 a까지 증가하는!
 
-	for (int i = 1; i <= a; i++) {
-		cin >> c;
-		if (c < b) {
-			cout << c << " ";
-		}
+	int x;
+	cin >> x;
 
-
-	}
-
+	if (x % 4 == 0 && x % 400 == 0)
+		cout << "1" << endl;
+	else if (x % 4 == 0 && x % 100 != 0)
+		cout << "1" << endl;
+	else
+		cout << "0" << endl;
 }
-
